5e281f75661c2b5dbf848803e7e0d126c95b28da
Johan Louwers <louwersj@gmail.com>
2015-02-01 16:48:29 +0100

    Update General_release_name.sql

--- Oracle_eBS_11/General_release_name.sql
+++ Oracle_eBS_11/General_release_name.sql
@@ -1,3 +1,13 @@
+/* 
+Name    :     General_release_name.sql
+Version :     V0.1
+Desc.   :     Query to select the release name from an Oracle eBS implementation
+              
+CHANGE LOG
+Version   Date      Developer                     Desc
+V0.1      01FEB2015   johan.louwers@capgemini.com   Initial upload to github
+ */
+
 select 
      release_name 
 from 
